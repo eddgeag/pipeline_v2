0a4d9033c8632b8e5ccf5f6f72057e53c01a3161
edmond geraud <eddgeag@gmail.com>
2024-07-12 11:42:18 -0500

    prubando cambiar compute depth 2

--- pipeline_v2/compute_depth.sh
+++ pipeline_v2/compute_depth.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
-echo("faidx")
+echo "compute faidx" 
 faidx ~/datos_exomas/datos_gatk/referencia/ensembl/referencia_filtered.fasta -i chromsizes >  chrom.sizes
-echo("encontrando tamanos de cromosomas canonicos")
+echo "encontrando tamanos de cromosomas canonicos" 
 awk '/^chr[0-9XY]*\t/ {printf("%s\t0\t%s\n",$1,$2);}' ~/datos_exomas/datos_gatk/referencia/ensembl/referencia_filtered.fasta.fai > bed_split.bed
-echo("cortar el bam")
+echo "cortar el bam" 
 samtools view -L bed_split.bed -o out.bam $1
-echo("sortear el bam a 16 gb")
+echo "sortear el bam a 16 gb" 
 samtools sort myfile.bam -@ 32 -o myfile_sorted.bam
-echo("sortear los chrom.sizes")
+echo "sortear los chrom.sizes" 
 cat chrom.sizes|sort -V > sizes.genome.sort
-echo("compuar cobertura")
+echo "compuar cobertura" 
 bedtools coverage -a ~/datos_exomas/coverage/hg38/xgen-exome.bed  -b out.bam -g sizes.genome.sort -sorted -hist 
